6c8e8d2934b2c556313975606010f0e9c7624bad
Kai Blin <kblin@biosustain.dtu.dk>
2016-09-20 09:49:23 +0200

    Add SQLAlchemy-related infrastructure to prepare for migration
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 
 DB_CONNECTION = "host='localhost' port=15432 user='postgres' password='secret' dbname='antismash'"
+SQLALCHEMY_DATABASE_URI = 'postgres://postgres:secret@localhost:15432/antismash'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+from .models import db
+
+db.init_app(app)
 
 from . import api
 from . import error_handlers
