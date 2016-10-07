02bdc87c98ece10832df8cc871c356d911869177
Kai Blin <kblin@biosustain.dtu.dk>
2016-10-07 13:22:58 +0200

    search: Limit available queries to first 50 hits, after sorting
    This makes the typeahead search for e.g. NCBI accessions actually
    usable.
    
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- api/search/available.py
+++ api/search/available.py
@@ -36,7 +36,7 @@ def available_term_by_category(category, term):
     cleaned_term = sanitise_string(term)
 
     if cleaned_category in AVAILABLE:
-        query = AVAILABLE[cleaned_category](cleaned_term)
+        query = AVAILABLE[cleaned_category](cleaned_term).limit(50)
         return map(lambda x: {'val': x[0], 'desc': x[1]}, query.all())
 
     return []
@@ -45,93 +45,121 @@ def available_term_by_category(category, term):
 @register_handler(AVAILABLE)
 def available_superkingdom(term):
     '''Generate query for available superkingdoms'''
-    return db.session.query(distinct(Taxa.superkingdom), null()).filter(Taxa.superkingdom.ilike('{}%'.format(term)))
+    return db.session.query(distinct(Taxa.superkingdom), null()) \
+             .filter(Taxa.superkingdom.ilike('{}%'.format(term))) \
+             .order_by(Taxa.superkingdom)
 
 
 @register_handler(AVAILABLE)
 def available_phylum(term):
     '''Generate query for available phyla'''
-    return db.session.query(distinct(Taxa.phylum), null()).filter(Taxa.phylum.ilike('{}%'.format(term)))
+    return db.session.query(distinct(Taxa.phylum), null()) \
+             .filter(Taxa.phylum.ilike('{}%'.format(term))) \
+             .order_by(Taxa.phylum)
 
 
 @register_handler(AVAILABLE)
 def available_class(term):
     '''Generate query for available class'''
-    return db.session.query(distinct(Taxa._class), null()).filter(Taxa._class.ilike('{}%'.format(term)))
+    return db.session.query(distinct(Taxa._class), null()) \
+             .filter(Taxa._class.ilike('{}%'.format(term))) \
+             .order_by(Taxa._class)
 
 
 @register_handler(AVAILABLE)
 def available_order(term):
     '''Generate query for available order'''
-    return db.session.query(distinct(Taxa.taxonomic_order), null()).filter(Taxa.taxonomic_order.ilike('{}%'.format(term)))
+    return db.session.query(distinct(Taxa.taxonomic_order), null()) \
+             .filter(Taxa.taxonomic_order.ilike('{}%'.format(term))) \
+             .order_by(Taxa.taxonomic_order)
 
 
 @register_handler(AVAILABLE)
 def available_family(term):
     '''Generate query for available family'''
-    return db.session.query(distinct(Taxa.family), null()).filter(Taxa.family.ilike('{}%'.format(term)))
+    return db.session.query(distinct(Taxa.family), null()) \
+             .filter(Taxa.family.ilike('{}%'.format(term))) \
+             .order_by(Taxa.family)
 
 
 @register_handler(AVAILABLE)
 def available_genus(term):
     '''Generate query for available genus'''
-    return db.session.query(distinct(Taxa.genus), null()).filter(Taxa.genus.ilike('{}%'.format(term)))
+    return db.session.query(distinct(Taxa.genus), null()) \
+             .filter(Taxa.genus.ilike('{}%'.format(term))) \
+             .order_by(Taxa.genus)
 
 
 @register_handler(AVAILABLE)
 def available_species(term):
     '''Generate query for available species'''
-    return db.session.query(distinct(Taxa.species), null()).filter(Taxa.species.ilike('{}%'.format(term)))
+    return db.session.query(distinct(Taxa.species), null()) \
+             .filter(Taxa.species.ilike('{}%'.format(term))) \
+             .order_by(Taxa.species)
 
 
 @register_handler(AVAILABLE)
 def available_strain(term):
     '''Generate query for available strain'''
-    return db.session.query(distinct(Taxa.strain), null()).filter(Taxa.strain.ilike('{}%'.format(term)))
+    return db.session.query(distinct(Taxa.strain), null()) \
+             .filter(Taxa.strain.ilike('{}%'.format(term))) \
+             .order_by(Taxa.strain)
 
 
 @register_handler(AVAILABLE)
 def available_acc(term):
     '''Generate query for available accession'''
-    return db.session.query(distinct(DnaSequence.acc), null()).filter(DnaSequence.acc.ilike('{}%'.format(term)))
+    return db.session.query(distinct(DnaSequence.acc), null()) \
+             .filter(DnaSequence.acc.ilike('{}%'.format(term))) \
+             .order_by(DnaSequence.acc)
 
 
 @register_handler(AVAILABLE)
 def available_compoundseq(term):
     '''Generate query for available compound by peptide sequence'''
-    return db.session.query(distinct(Compound.peptide_sequence), null()).filter(Compound.peptide_sequence.ilike('{}%'.format(term)))
+    return db.session.query(distinct(Compound.peptide_sequence), null()) \
+             .filter(Compound.peptide_sequence.ilike('{}%'.format(term))) \
+             .order_by(Compound.peptide_sequence)
 
 
 @register_handler(AVAILABLE)
 def available_compoundclass(term):
     '''Generate query for available compound by class'''
-    return db.session.query(distinct(Compound._class), null()).filter(Compound._class.ilike('{}%'.format(term)))
+    return db.session.query(distinct(Compound._class), null()) \
+             .filter(Compound._class.ilike('{}%'.format(term))) \
+             .order_by(Compound._class)
 
 
 @register_handler(AVAILABLE)
 def available_monomer(term):
     '''Generate query for available monomer'''
-    return db.session.query(distinct(Monomer.name), Monomer.description).filter(or_(Monomer.name.ilike('{}%'.format(term)), Monomer.description.ilike('{}%'.format(term))))
+    return db.session.query(distinct(Monomer.name), Monomer.description) \
+             .filter(or_(Monomer.name.ilike('{}%'.format(term)), Monomer.description.ilike('{}%'.format(term)))) \
+             .order_by(Monomer.name)
 
 
 @register_handler(AVAILABLE)
 def available_type(term):
     '''Generate query for available type'''
-    return db.session.query(distinct(BgcType.term), BgcType.description).filter(or_(BgcType.term.ilike('{}%'.format(term)), BgcType.description.ilike('{}%'.format(term))))
+    return db.session.query(distinct(BgcType.term), BgcType.description) \
+             .filter(or_(BgcType.term.ilike('{}%'.format(term)), BgcType.description.ilike('{}%'.format(term)))) \
+             .order_by(BgcType.term)
 
 
 @register_handler(AVAILABLE)
 def available_profile(term):
     '''Generate query for available asDomain profile'''
     return db.session.query(distinct(Profile.name), Profile.description) \
-             .filter(or_(Profile.name.ilike('{}%'.format(term)), Profile.description.ilike('%{}%'.format(term))))
+             .filter(or_(Profile.name.ilike('{}%'.format(term)), Profile.description.ilike('%{}%'.format(term)))) \
+             .order_by(Profile.name)
 
 
 @register_handler(AVAILABLE)
 def available_asdomain(term):
     '''Generate query for available asDomain profile'''
     return db.session.query(distinct(AsDomainProfile.name), AsDomainProfile.description) \
-             .filter(or_(AsDomainProfile.name.ilike('{}%'.format(term)), AsDomainProfile.description.ilike('%{}%'.format(term))))
+             .filter(or_(AsDomainProfile.name.ilike('{}%'.format(term)), AsDomainProfile.description.ilike('%{}%'.format(term)))) \
+             .order_by(AsDomainProfile.name)
 
 
 @register_handler(AVAILABLE)
@@ -139,7 +167,8 @@ def available_clusterblast(term):
     '''Generate query for available ClusterBlast hits'''
     return db.session.query(distinct(ClusterblastHit.acc), ClusterblastHit.description) \
              .join(ClusterblastAlgorithm).filter(ClusterblastAlgorithm.name == 'clusterblast') \
-             .filter(or_(ClusterblastHit.acc.ilike('{}%'.format(term)), ClusterblastHit.description.ilike('%{}%'.format(term))))
+             .filter(or_(ClusterblastHit.acc.ilike('{}%'.format(term)), ClusterblastHit.description.ilike('%{}%'.format(term)))) \
+             .order_by(ClusterblastHit.acc)
 
 
 @register_handler(AVAILABLE)
@@ -147,7 +176,8 @@ def available_knowncluster(term):
     '''Generate query for available KnownClusterBlast hits'''
     return db.session.query(distinct(ClusterblastHit.acc), ClusterblastHit.description) \
              .join(ClusterblastAlgorithm).filter(ClusterblastAlgorithm.name == 'knownclusterblast') \
-             .filter(or_(ClusterblastHit.acc.ilike('{}%'.format(term)), ClusterblastHit.description.ilike('%{}%'.format(term))))
+             .filter(or_(ClusterblastHit.acc.ilike('{}%'.format(term)), ClusterblastHit.description.ilike('%{}%'.format(term)))) \
+             .order_by(ClusterblastHit.acc)
 
 
 @register_handler(AVAILABLE)
@@ -155,4 +185,5 @@ def available_subcluster(term):
     '''Generate query for available SubClusterBlast hits'''
     return db.session.query(distinct(ClusterblastHit.acc), ClusterblastHit.description) \
              .join(ClusterblastAlgorithm).filter(ClusterblastAlgorithm.name == 'subclusterblast') \
-             .filter(or_(ClusterblastHit.acc.ilike('{}%'.format(term)), ClusterblastHit.description.ilike('%{}%'.format(term))))
+             .filter(or_(ClusterblastHit.acc.ilike('{}%'.format(term)), ClusterblastHit.description.ilike('%{}%'.format(term)))) \
+             .order_by(ClusterblastHit.acc)
