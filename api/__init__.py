025147d2710b33fdf99f2aa867bfabf7c43f3b27
Kai Blin <kblin@biosustain.dtu.dk>
2018-10-12 17:14:38 +0200

    chore: Add log output capturing request timings
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask
+import time
+from flask import Flask, g, request
 
 SQLALCHEMY_DATABASE_URI = os.getenv('AS_DB_URI', 'postgres://postgres:secret@localhost:5432/antismash')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -10,6 +11,29 @@ app.config.from_object(__name__)
 from .models import db
 
 db.init_app(app)
+
+
+@app.before_request
+def start_timer():
+    g.start = time.time()
+
+@app.after_request
+def log_request(response):
+    now = time.time()
+    duration = round(now - g.start, 2)
+
+    log_params = [
+        ('method', request.method),
+        ('path', request.path),
+        ('status', response.status_code),
+        ('duration', duration),
+    ]
+
+    line = " ".join(["{}={}".format(name, value) for name, value in log_params])
+    app.logger.info(line)
+
+    return response
+
 
 from . import api
 from . import error_handlers
